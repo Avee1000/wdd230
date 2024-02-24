cf11d9862c784016df39e69578c3ac1c2ed4f58a
Avee1000 <eddyidahosa01@gmail.com>
2024-02-24 16:21:31 +0100

    discover

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -24,3 +24,33 @@ modified.innerHTML += lastMod;
 // // Update the content of the 'visit' element with the current visit count
 // document.getElementById('visit').innerText = `Page Visits: ` + localStorage.getItem('visitCount');
 
+// Check if localStorage has a 'lastVisit' key
+    
+const visit = document.getElementById("last-visit");
+visit.style.fontSize = "1.3rem";
+
+if(localStorage.getItem('lastVisit') === null) {
+    // First visit
+    visit.innerText = "Welcome! Let us know if you have any questions.";
+    } else {
+    // Get the last visit date from localStorage
+    const lastVisit = new Date(localStorage.getItem('lastVisit'));
+
+    // Calculate the time difference in milliseconds
+    const timeDifference = new Date() - lastVisit;
+
+    // Calculate the number of days
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (daysDifference < 1) {
+        // Less than a day
+       visit.innerText = "Back so soon! Awesome!";
+    } else {
+        // Display the number of days
+        const message = (daysDifference === 1) ? "day" : "days";
+        visit.innerText = "You last visited " + daysDifference + " " + message + " ago.";
+    }
+}
+
+// Update localStorage with the current visit date
+localStorage.setItem('lastVisit', new Date().toString());
