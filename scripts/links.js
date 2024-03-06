ab7106ea9896bdba864fdd5df80ef30563fcb0f4
Avee1000 <eddyidahosa01@gmail.com>
2024-03-06 15:47:04 +0100

    json

--- scripts/links.js
+++ scripts/links.js
@@ -1,5 +1,7 @@
 const linksURL = "https://avee1000.github.io/wdd230/data/links.json";
 
+const unorderedList = document.getElementById('links');
+
 // const links = unorderedList.querySelectorAll('.card ul li');
     
 async function getLinks() {
@@ -12,13 +14,30 @@ async function getLinks() {
 
 function displayLinks(lessons) {
 
-        const length = lessons.length;
-        const unorderedList = document.getElementById('links');
+    for (let i = 0; i < lessons.length; i++) {
+
+        const length = lessons[i].links.length;
         const links = document.createElement('li');
+        links.innerHTML = `Week ${lessons[i].week}: `;
 
-        links.innerHTML = `Week ${length}:`;
         unorderedList.appendChild(links);
+        
+        for (let j = 0; j < lessons[i].links.length; j++) {
 
+            const week1 = document.createElement('a');
+            week1.setAttribute('href', `${lessons[i].links[j].url}`);
+            week1.setAttribute('target', "_blank");
+            week1.innerHTML = `${lessons[i].links[j].title}`;
+            links.appendChild(week1);          
+
+            if (j < length - 1) {
+                let span = document.createElement("span");
+                span.innerHTML = ` | `;
+                links.appendChild(span);
+            }
+        }
+    }
+    
 }
 
 getLinks();
