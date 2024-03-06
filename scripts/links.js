1d9170a856a039b5341a5a955dd1971842f2214c
Avee1000 <eddyidahosa01@gmail.com>
2024-03-06 13:48:29 +0100

    hehe

--- scripts/links.js
+++ scripts/links.js
@@ -1,14 +1,24 @@
-const baseURL = "https://avee1000.github.io/wdd230/";
 const linksURL = "https://avee1000.github.io/wdd230/data/links.json";
 
-async function getLinkList() {
+// const links = unorderedList.querySelectorAll('.card ul li');
     
+async function getLinks() {
     const response = await fetch(linksURL);
-    const data = response.json();
-    console.log(data);
+    if (response.ok) {
+        const data = await response.json();
+        displayLinks(data.lessons);
+    }
+}
 
-    // displayLinks(data.lessons);
+function displayLinks(lessons) {
+
+        const length = lessons.length;
+        const unorderedList = document.getElementById('links');
+        const links = document.createElement('li');
+
+        links.innerHTML = `Week ${length}:`;
+        unorderedList.appendChild(links);
 
 }
 
-getLinksList();
+getLinks();
