c91bd9842fe41188dcb6238af7f4c62c45399af7
Avee1000 <eddyidahosa01@gmail.com>
2024-03-12 15:49:31 +0100

    sas

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,9 +2,7 @@ const currentTemp = document.getElementById('current-temp');
 
 const weatherIcon = document.getElementById('weather-icon');
 
-const captionDesc = document.querySelector('figcaption');
-
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&APPID=61a0af6c0abff327e78c6ce5bfbb578c";
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=6.3&lon=5.6&appid=61a0af6c0abff327e78c6ce5bfbb578c";
 
 async function apiFetch() {
     try {
@@ -27,11 +25,7 @@ function displayWeather(data) {
     currentTemp.innerHTML = `${data.main.temp.toFixed(2)}`;
 
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-    let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', "weather");
-    captionDesc.innerHTML = `${desc.toUpperCase()}`;
-    
-
+    weatherIcon.setAttribute('alt', "weather");    
 
 }
