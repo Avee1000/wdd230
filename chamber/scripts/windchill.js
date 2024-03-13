a4bda2e61c2725227d3282a3cc7084e6f2d07747
Avee1000 <eddyidahosa01@gmail.com>
2024-03-13 16:57:48 +0100

    weather

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,16 +1,65 @@
-const temperature = document.getElementById('temperature');
-let newTemperature = 50;
-temperature.innerHTML += newTemperature + `&deg;F`;
+// const temperature = document.getElementById('temperature');
+// let newTemperature = 50;
+// temperature.innerHTML += newTemperature + `&deg;F`;
 
-const speed = document.getElementById('speed');
-let newSpeed = 4;
-speed.innerHTML += newSpeed + ` km/h`;
+// const speed = document.getElementById('speed');
+// let newSpeed = 4;
+// speed.innerHTML += newSpeed + ` km/h`;
 
-function windchill(t, s) {
-    let windChill = (35.74 + (0.6215 * t)) - ((35.75) * (s ** 0.16)) + ((0.4275 * t * (s ** 0.16)))
-    return windChill;
+// function windchill(t, s) {
+//     let windChill = (35.74 + (0.6215 * t)) - ((35.75) * (s ** 0.16)) + ((0.4275 * t * (s ** 0.16)))
+//     return windChill;
+// }
+
+// const chill = document.getElementById('chill');
+// let windChill = windchill(newTemperature, newSpeed);
+// chill.innerHTML += windChill.toFixed() + `&deg;F`;
+
+const currentTemp = document.getElementById('current-temp');
+
+const currentDesc = document.getElementById('description');
+
+const weatherIcon = document.getElementById('weather-icon');
+
+const div = document.querySelector('#more-info');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=6.32691&lon=5.60750&units=imperial&APPID=61a0af6c0abff327e78c6ce5bfbb578c";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayWeather(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-const chill = document.getElementById('chill');
-let windChill = windchill(newTemperature, newSpeed);
-chill.innerHTML += windChill.toFixed() + `&deg;F`;
+apiFetch();
+
+function displayWeather(data) {
+
+    currentTemp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+
+    const desc = data.weather[0].description;
+
+    currentDesc.innerHTML = `- ${desc.charAt(0).toUpperCase() + desc.slice(1)}`;
+
+    const humidity = document.createElement('p');
+    humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
+    div.appendChild(humidity);
+
+    const windSpeed = document.createElement('p');
+    windSpeed.innerHTML = `Wind Speed: ${data.wind.speed}km/h`;
+    div.appendChild(windSpeed);
+
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', "weather");    
+}
+
+document.getElementById("banner").addEventListener("load")
