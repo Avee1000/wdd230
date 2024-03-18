6f51a344a52bc3f5f828043b8e818d678ada1138
Avee1000 <eddyidahosa01@gmail.com>
2024-03-18 15:52:11 +0100

    sada

--- chamber/scripts/random.js
+++ chamber/scripts/random.js
@@ -1,10 +1,10 @@
-const url = "https://avee1000.github.io/wdd230/chamber/data/members.json";
+const newUrl = "https://avee1000.github.io/wdd230/chamber/data/members.json";
 
 const spotlight = document.getElementById("spot-ad");
 
 async function getRanMembersData(){
     try {
-        const response = await fetch(url);
+        const response = await fetch(newUrl);
         if (response.ok) {
             const data = await response.json();
             displayRanMembers(data.companies);
@@ -23,12 +23,11 @@ function displayRanMembers(companies) {
 
     const needed = [];
 
-    companies.forEach((company) => {
-
-        if (company.memberLevel == "Gold" || company.memberLevel == "Silver") {
-            needed.push(company); 
+    for (let i = 0; i < companies.length; i++) {
+        if (companies[i].memberLevel === "Gold" || companies[i].memberLevel === "Silver") {
+            needed.push(companies[i]); 
         }
-    });
+    }
 
     needed.forEach((x) => {
         const article = document.createElement("article");
@@ -36,6 +35,26 @@ function displayRanMembers(companies) {
         const name = document.createElement("h3");
         name.innerHTML = `${x.name}`;
 
-        article.appendChild(name);
+        const logo = document.createElement("img");
+        logo.setAttribute('src', x.image);
+        logo.setAttribute('alt', x.name);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('height', '50');
+        logo.setAttribute('width', '80');
+        
+        const address = document.createElement("p");
+        address.innerHTML = `${x.address}`;
+
+        const phone = document.createElement("p");
+        phone.innerHTML = `${x.phone}`
+        
+        const url = document.createElement("a");
+        url.innerHTML = `${x.url}`;
+        url.style.color = "blue";
+        url.setAttribute('href', x.url)
+        url.setAttribute('target', '_blank')       
+
+        article.append(name, logo, address, phone, url );
+        spotlight.appendChild(article);
     });
  }
