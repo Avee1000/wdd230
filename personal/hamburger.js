246045641c3b352fdebf599e142e0b0d60f7e2ff
Avee1000 <eddyidahosa01@gmail.com>
2024-03-27 14:00:55 +0100

    personal

--- personal/hamburger.js
+++ personal/hamburger.js
@@ -1,25 +1,28 @@
-window.addEventListener("load", () => {
-    document.body.style.overflow = "hidden";
-    document.getElementById("myNav").style.width = "100%";
-});
-
-document.querySelector("#close").addEventListener("click", () => {
-    document.body.style.overflow = "";
-    document.querySelector("#myNav").style.height = "0";
-});
-
-const hidden = document.getElementById("hidden-links");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-// hamburgerMenu.addEventListener("click", () => {
-//     event.stopPropagation(); // Prevent the click event from reaching the window click listener
-//     hidden.style.display = (hidden.style.display === "none") ? "block" : "none";
+// window.addEventListener("load", () => {
+//     document.body.style.overflow = "hidden";
+//     document.getElementById("myNav").style.width = "100%";
 // });
 
-// Add a click event listener to the window to close the element when clicking outside
-window.addEventListener("click", () => {
-    hidden.style.display = "none";
+// document.querySelector("#close").addEventListener("click", () => {
+//     document.body.style.overflow = "";
+//     document.querySelector("#myNav").style.height = "0";
+// });
+
+
+const mainNav = document.querySelector('.navigation');
+const hamburger = document.querySelector("#menu");
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    // hamburger.classList.toggle('show');
+    nav.style.height = "100%";
 });
+
+document.querySelector(".delete").addEventListener("click", () => {
+    nav.style.height = "";
+});
+
 
 // JavaScript to scroll to the top of the page when the "Back to top" link is clicked:
 const scrollToTop = document.querySelector(".accordion");
@@ -27,3 +30,17 @@ const scrollToTop = document.querySelector(".accordion");
 scrollToTop.addEventListener("click", () => {
     window.scrollTo({top: 0, behavior: 'smooth'});
 });
+
+
+// const hidden = document.getElementById("hidden-links");
+// const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+// hamburgerMenu.addEventListener("click", () => {
+//     event.stopPropagation(); // Prevent the click event from reaching the window click listener
+//     hidden.style.display = (hidden.style.display === "none") ? "block" : "none";
+// });
+
+// Add a click event listener to the window to close the element when clicking outside
+// window.addEventListener("click", () => {
+//     hidden.style.display = "none";
+// });
