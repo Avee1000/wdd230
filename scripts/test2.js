ef8eb233ba21495fecd16dcc7ddd97224a31a2b3
Avee1000 <eddyidahosa01@gmail.com>
2025-01-15 12:29:55 +0100

    Add new images and test2.js with car object

---
+++ scripts/test2.js
@@ -0,0 +1,11 @@
+let car = {
+    color: 'Red',
+    make: 'Toyota',
+    model:'Camry',
+    drive: function() {
+        console.log("The car is drving");
+    }
+};
+
+console.log(car.color);
+car.drive();
